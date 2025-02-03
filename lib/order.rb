92d08c576c0e2e7ac106e9eb75833c23e4d94689
Radheshyam Rathore <rsrathore002@gmail.com>
2025-02-03 13:50:29 +0530

    add pizzas in order and also add side items and its calculation for total price

--- lib/order.rb
+++ lib/order.rb
@@ -6,4 +6,29 @@ class Order
     "Cold drink" => 55,
     "Mousse cake" => 90
   }
+
+  def initialize
+    @pizzas = []
+    @sides = []
+  end
+
+  def add_pizza(pizza)
+    @pizzas << pizza
+  end
+
+  def add_side(side)
+    return "Invalid side item" unless SIDE_ITEMS.key?(side)
+
+    @sides << side
+  end
+
+  def total_price
+    pizza_cost = @pizzas.map(&:total_price).sum
+    side_cost = @sides.map { |s| SIDE_ITEMS[s] }.sum
+    pizza_cost + side_cost
+  end
+
+  def confirm
+    "Order placed successfully! Total amount: Rs. #{total_price}"
+  end
 end
