bbd414993a6a117dcbf9e532e5e1681ce24ebc30
Radheshyam Rathore <rsrathore002@gmail.com>
2025-02-03 12:56:47 +0530

    add toppings for vegetarian and non vegetarian

--- lib/pizza.rb
+++ lib/pizza.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Pizza
   attr_accessor :name, :size, :crust, :toppings, :extra_cheese, :base_price
 
@@ -38,12 +39,50 @@ class Pizza
     validate_crust
   end
 
+  def add_topping(topping)
+    return "Invalid topping" unless valid_topping?(topping)
+
+    if @size == :large && @toppings.size < 2
+      @toppings << topping  # Large pizzas allow 2 free toppings
+    elsif @size == :large && @toppings.size >= 2
+      return "Cannot add more than 2 toppings for Large size."
+    else
+      @toppings << topping
+    end
+  end
+
+  def add_extra_cheese
+    @extra_cheese = true
+  end
+
   private
+
+  def valid_topping?(topping)
+    return false unless VEG_TOPPINGS.keys.include?(topping) || NON_VEG_TOPPINGS.keys.include?(topping)
+
+    if vegetarian? && NON_VEG_TOPPINGS.keys.include?(topping)
+      return false # Vegetarian pizzas cannot have non-veg toppings
+    elsif !vegetarian? && topping == "Paneer"
+      return false # Non-veg pizzas cannot have Paneer
+    elsif !vegetarian? && NON_VEG_TOPPINGS.keys.include?(topping) && @toppings.any? { |t| NON_VEG_TOPPINGS.keys.include?(t) }
+      return false # Non-veg pizzas can only have one non-veg topping
+    end
+
+    true
+  end
 
   def validate_crust
     unless CRUST_TYPES.include?(@crust)
       raise "Invalid crust type selected!"
     end
+  end
+
+  def vegetarian?
+    ["Deluxe Veggie", "Cheese and Corn", "Paneer Tikka"].include?(@name)
+  end
+
+  def topping_price(topping)
+    VEG_TOPPINGS[topping] || NON_VEG_TOPPINGS[topping] || 0
   end
 end
   
